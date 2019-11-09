992e10032ef5e4a6daa862bec5d166521ea5b25d
Ivaylo Apostolov <ivaylo.apostolov@gmail.com>
2019-11-09 20:04:46 +0200

    basics

--- src/App.js
+++ src/App.js
@@ -1,33 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Input from './Input/Input';
-import Output from './Output/Output'
+import Person from './Person/Person';
+//import { ReactComponent } from '*.svg';
 
-class App extends React.Component {
+class App extends Component {
   state = {
-    name: "Ivaylo",
-    age: 35
+    person: [
+      {
+        name: "Ivaylo",
+        age: 35
+      },
+      {
+        name: "Toma",
+        age: 32
+      }
+    ],
+    otherPersons: [
+      {
+        name: "Goshko",
+        age: 21
+      }
+    ]
   }
 
-  nameHandler = (event) => {
-    this.setState(
-      {
-        name: event.target.value,
-        age: 35
-      }
-    )
+  changeNameHandler = (newName) => {
+    this.setState({
+      otherPersons: [
+        {
+          name: newName,
+          age: 34
+        }
+      ]
+    })
+  }
+
+  nameChangeHandelr = (event) => {
+    this.setState({
+      person: [
+        {
+          name: event.target.value,
+          age: 35
+        },
+        {
+          name: "Toma",
+          age: 32
+        }
+      ]})
   }
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid grey",
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
-        <Input changed={this.nameHandler}/>
-        <Output 
-        name={this.state.name} 
-        age={this.state.age}
+        <h1>Ivaylo</h1>
+        <p>React dev</p>
+        <button 
+        style={style}
+        onClick={this.changeNameHandler.bind(this, "Gogo")}>Change the Name</button>
+        <Person
+          name={this.state.person[0].name}
+          age={this.state.person[0].age}
+          changed={this.nameChangeHandelr}>Hobby: tapout
+        </Person>
+        <Person
+          name={this.state.person[1].name}
+          age={this.state.person[1].age}
+          
         />
-        <Output />
-        <Output />
+        <Person
+          name={this.state.otherPersons[0].name}
+          age={this.state.otherPersons[0].age}
+          click={this.changeNameHandler.bind(this, "Gogo")}
+        />
       </div>
     )
   }
