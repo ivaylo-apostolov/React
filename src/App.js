5ea280de4219fb83c9745344d84b429cd61384e3
ivaylo-apostolov <ivaylo.apostolov@gmail.com>
2019-11-11 00:33:16 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 //import { ReactComponent } from '*.svg';
 
 class App extends Component {
@@ -8,79 +8,94 @@ class App extends Component {
     person: [
       {
         name: "Ivaylo",
-        age: 35
+        age: 35,
+        id:'asdf'
       },
       {
         name: "Toma",
-        age: 32
+        age: 32,
+        id:'jlkjlkj'
       }
     ],
     otherPersons: [
       {
         name: "Goshko",
-        age: 21
+        age: 21,
+        id:'jlkjlkj'
       }
-    ]
+    ],
+    buttonPushed: false
+  };
+
+  deletePerson = (personIndex) => {
+    let updatedPersons = this.state.person;
+    updatedPersons.splice(personIndex, 1);
+    this.setState({person: updatedPersons})
   }
 
-  changeNameHandler = (newName) => {
-    this.setState({
-      otherPersons: [
-        {
-          name: newName,
-          age: 34
-        }
-      ]
-    })
-  }
+  nameChangeHandelr = (event, id) => {
+    const personIndex = this.state.person.findIndex(p => p.id === id)
+    let person = {...this.state.person[personIndex]};
+    person.name = event.target.value;
+    const personsArr = [...this.state.person];
+    personsArr[personIndex] = person;
 
-  nameChangeHandelr = (event) => {
     this.setState({
-      person: [
-        {
-          name: event.target.value,
-          age: 35
-        },
-        {
-          name: "Toma",
-          age: 32
-        }
-      ]})
-  }
+      person:personsArr
+    });
+  };
+
+  triggerNameChange = () => {
+    this.setState({
+      buttonPushed: !this.state.buttonPushed
+    });
+  };
 
   render() {
     const style = {
       backgroundColor: "white",
       font: "inherit",
       border: "1px solid grey",
-      padding: '8px',
-      cursor: 'pointer'
+      padding: "8px",
+      cursor: "pointer"
+    };
+
+    let persons = null;
+
+    if (this.state.buttonPushed) {
+      persons = (
+        <div>
+          {this.state.person.map((person, index) => {
+            return <Person 
+            click ={() => this.deletePerson(index)}
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangeHandelr(event, person.id)}
+            />
+          })
+          }
+          {this.state.otherPersons.map(person => {
+            return <Person 
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangeHandelr2(event, person.id)} />
+          })}
+        </div>
+      );
     }
 
     return (
       <div className="App">
         <h1>Ivaylo</h1>
         <p>React dev</p>
-        <button 
-        style={style}
-        onClick={this.changeNameHandler.bind(this, "Gogo")}>Change the Name</button>
-        <Person
-          name={this.state.person[0].name}
-          age={this.state.person[0].age}
-          changed={this.nameChangeHandelr}>Hobby: tapout
-        </Person>
-        <Person
-          name={this.state.person[1].name}
-          age={this.state.person[1].age}
-          
-        />
-        <Person
-          name={this.state.otherPersons[0].name}
-          age={this.state.otherPersons[0].age}
-          click={this.changeNameHandler.bind(this, "Gogo")}
-        />
+        <button style={style} onClick={this.triggerNameChange}>
+          Change the Name
+        </button>
+        {persons}
       </div>
-    )
+    );
   }
 }
 
